ff92e7c5fc8f8ee24646fea9478be160b296e8c1
Elise St Hilaire and grinil khanna <grinil@>
2017-01-10 15:55:52 -0800

    Add divs to create flashcards

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,142 @@
 $(function() {
   $(".clickable").click(function() {
-  $(".demoWord").toggle();
-  $(".definition").show();
-    });
+    $("#javascript").toggle();
+    $("#defJavascript").toggle();
 });
+  $(".clickable").click(function() {
+    $("#operators").toggle();
+    $("#defOperators").toggle();
+});
+
+});
+
+
+// });
+//
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#operators").toggle();
+//   $("#defOperators").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#variables").toggle();
+//   $("#defVariables").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#variableNaming").toggle();
+//   $("#defVarNaming").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#functions").toggle();
+//   $("#deFunctions").show();
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#methods").toggle();
+//   $("#defMethods").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#arguments").toggle();
+//   $("#defArguments").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#parameters").toggle();
+//   $("#defParameters").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#return").toggle();
+//   $("#defReturn").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#chainingMethods").toggle();
+//   $("#defChaining").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#string").toggle();
+//   $("#defString").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#boolean").toggle();
+//   $("#defBoolean").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#undefined").toggle();
+//   $("#defUndefined").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#NaN").toggle();
+//   $("#defNaN").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#escape").toggle();
+//   $("#defEscape").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#alert").toggle();
+//   $("#defAlert").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#comments").toggle();
+//   $("#defComments").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#JQuery").toggle();
+//   $("#defJQuery").show();
+//     });
+// });
+//
+// $(function() {
+//   $(".clickable").click(function() {
+//   $("#attributes").toggle();
+//   $("#defAttributes").show();
+//     });
+// });
+//
+// });
